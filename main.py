6060ddc081932facca6d65155d246a743723da81
Sachin <sachingupta9997631@gmail.com>
2023-03-13 16:30:11 +0530

    all files are deleted

--- main.py
+++
@@ -1,8 +0,0 @@
-def Add(a, b):
-        return a + b
-        
-def SayHello():
-        print("Hello world from Sachin Gupta")
-
-if __name__ == '__main__':
-        SayHello()
--- readme.md
+++
@@ -1,2 +0,0 @@
-# demo_repo
-# demo_repo for CI/CD
