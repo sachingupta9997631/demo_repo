6f2dcbfcb8f188873118f50774163d0a670bb8dc
Sachin <sachingupta9997631@gmail.com>
2023-03-13 23:33:18 +0530

    added test case for cube

--- test_main.py
+++ test_main.py
@@ -3,6 +3,7 @@ from main import cube
 def TestCube():
     assert cube(1) == 1
     assert cube(2) == 8
+    assert cube(3) == 27
     print("the cube function is Tested Successfully")
 
 if __name__ == "__main__":
